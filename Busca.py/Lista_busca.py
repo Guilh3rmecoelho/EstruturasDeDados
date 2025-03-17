6b8fabde0fe2a991fa0de72433e2d2edbdf6052e
G_Coelho_D <129126103+Guilh3rmecoelho@users.noreply.github.com>
2025-03-17 05:37:24 -0300

    Update Lista_busca.py

--- Busca.py/Lista_busca.py
+++ Busca.py/Lista_busca.py
@@ -13,7 +13,5 @@ fim = time.time()
 print(f"Tempo de execução sem estrutura otimizada: {fim - inicio:.4f} segundos\n")
 
 
-# Explicação:
+
 # No primeiro caso, usamos uma lista, onde a busca por um elemento exige percorrer toda a lista em média O(n) tempo.
-# No segundo caso, usamos um conjunto (set), que utiliza uma tabela hash, permitindo buscas em tempo O(1).
-# Isso torna o segundo código muito mais eficiente para grandes volumes de dados.
